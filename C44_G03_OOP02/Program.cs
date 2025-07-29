3abfbf39db69c57b39f1316e25d1b6c2d224c9b3
Mahmoud Mansour <mansour@gmail.com>
2025-07-29 20:05:33 +0300

    Add project files.

---
+++ C44_G03_OOP02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace C44_G03_OOP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Struct
+
+
+
+            #endregion
+        }
+    }
+}
