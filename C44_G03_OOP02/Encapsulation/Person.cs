4bf14c19c4e2fd8e28ff5eb192ade18f5d2a9ae4
Mahmoud Mansour <mansour@gmail.com>
2025-07-30 22:48:10 +0300

    Complete Q01

---
+++ C44_G03_OOP02/Encapsulation/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G03_OOP02.Encapsulation
+{
+    internal struct Person
+    {
+        #region Attributes
+        private string name;
+        private int age;
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value is not null && value.Length != 0)
+                    name = value;
+                else
+                    Console.WriteLine("Invalid Name.");
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >= 12)
+                    age = value;
+                else
+                    Console.WriteLine("invalid Age");
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        #endregion
+    }
+}
--- C44_G03_OOP02/Program.cs
+++ C44_G03_OOP02/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace C44_G03_OOP02
+﻿using C44_G03_OOP02.Encapsulation;
+
+namespace C44_G03_OOP02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            #region Struct
+            #region 1.	Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            //Person[] person = new Person[3];
+
+            //person[0] = new Person("Ahmed", 22);
+            //person[1] = new Person("Omar", 25);
+            //person[2] = new Person("Ali", 27);
+
+            //for (int i = 0; i < person.Length; i++)
+            //{
+            //    Console.WriteLine($"Name: {person[i].Name}, Age: {person[i].Age}");
+            //}
 
 
 
